1a1f9a697b900de42ece28609432833932acfbc3
Ronah Jay Emperio <ronahjay.emperio.20@usjr.edu.ph>
2022-11-10 13:16:57 +0800

    add functionality method

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -1,7 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
+    } else {
+      showAlert(true, 'success', 'item added to the list');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
   const showAlert = (show = false, type = '', msg = '') => {
     setAlert({ show, type, msg });
   };
